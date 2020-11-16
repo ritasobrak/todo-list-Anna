4b6508c36f19bf769743856883ef5fbf03dda2de
ritasobrak <margarita.sobrak@khk.ee>
2020-11-16 13:37:38 +0000

    Added "clear tasks" button, added clear all tasks function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('#clear-tasks');
 
 //define event listeners
 // add task to list - submit button
@@ -9,6 +10,9 @@ form.addEventListener('submit', addTask);
 
 //remove task from list - fas fa-backspace icon
 taskList.addEventListener('click', removeTask);
+
+//clear all tasks from list
+clearBtn.addEventListener('click', clearTasks);
 
 
 //addTask function
@@ -42,10 +46,15 @@ function addTask(e) {
 
 //removeTask function
 function removeTask(e) {
-    //is click is over icon
+    //is click is over icon - over a tag
     if (e.target.parentElement.classList.contains('secondary-content')) {
         if (confirm('Do you want to remove this task?')) {
             e.target.parentElement.parentElement.remove();
         }
     }
+}
+
+//clearTasks function
+function clearTasks(e) {
+    taskList.innerHTML = '';
 }
